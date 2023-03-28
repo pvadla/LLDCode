15940141c1d935efb1e1b151faa97a98eabacbf3
pvadla <praveen454griet@gmail.com>
2023-03-28 22:48:32 +0530

    0328

---
+++ src/main/java/com/scaler/bmsfair/BmsfairApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.bmsfair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsfairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsfairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/bmsfair/BmsfairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.bmsfair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsfairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
